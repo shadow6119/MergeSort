a115d25ec053f5b0bd02495fe5097264cc85d92b
BuildTools <unconfigured@null.spigotmc.org>
2019-06-22 13:31:05 +0900

    こつせつからの宿題完了

---
+++ Sort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+	public static class Sort
+	{
+		public static List<int> MergeSort(List<int> tergetList)
+		{
+			int length = tergetList.Count;//対象の長さ
+			int mid = length / 2; //真ん中をとる
+
+			if (length == 1) return tergetList; //要素一つなら帰る
+			
+			List<int> leftList = MergeSort( tergetList.GetRange(0,mid));
+			List<int> rightList = MergeSort( tergetList.GetRange(mid, length-mid));
+			
+			List<int> mergedList = new List<int>();
+
+			while (leftList.Count != 0 && rightList.Count != 0)
+			{
+				if (leftList[0] < rightList[0])
+				{
+					mergedList.Add(leftList[0]);
+					leftList.RemoveAt(0);
+				}
+				else
+				{
+					mergedList.Add(rightList[0]);
+					rightList.RemoveAt(0);
+				}
+			}
+
+			if (leftList.Count > 0)
+			{
+				mergedList.AddRange(leftList);
+			}
+			else
+			{
+				mergedList.AddRange(rightList);
+			}
+
+			return mergedList;
+
+		}
+	}
+}
